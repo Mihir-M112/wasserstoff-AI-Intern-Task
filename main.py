c3fa4f06fff2c951c8f43843a35adb50c467e302
Mihir Mishra <mihir.mishra988@gmail.com>
2024-10-14 15:36:55 +0530

    data_ingestion

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# For data ingestion run this command
+
+#  from pdf_pipeline.data_ingestions import run_pipeline
+# run_pipeline()
+
 from pdf_pipeline.pdf_processor import run_pipeline
 
 run_pipeline("Datasets")
